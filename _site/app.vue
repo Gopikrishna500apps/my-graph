b5397dd61336f1a20b866f0ffd729fc436fa9e77
Gopikrishna-Gurivisetti <gopikrishna.500apps@gmail.com>
2023-11-06 15:30:26 +0530

    sixth commit

---
+++ _site/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <index />
+  </div>
+</template>
+<script setup>
+import index from '@/pages/index.vue'
+</script>
---
+++ _site/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+<NetworkGraph :chartData="chartData" />
+    </div>
+</template>
+<script setup>
+import NetworkGraph from '../src/components/NetworkGraph.vue';
+import chartData from '../src/data/networkGraph.json';
+</script>
