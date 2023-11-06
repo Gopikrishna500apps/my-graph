d603e6cb0b05041cc922079f96a123676c51d2f6
Gopikrishna-Gurivisetti <gopikrishna.500apps@gmail.com>
2023-11-06 15:38:16 +0530

    Remove site folder

--- _site/app.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <index />
-  </div>
-</template>
-<script setup>
-import index from '@/pages/index.vue'
-</script>
--- _site/pages/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-<NetworkGraph :chartData="chartData" />
-    </div>
-</template>
-<script setup>
-import NetworkGraph from '../src/components/NetworkGraph.vue';
-import chartData from '../src/data/networkGraph.json';
-</script>
